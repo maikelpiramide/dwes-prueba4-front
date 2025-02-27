bc465fa1903115d14dbc0b8f5873d12ad247f5d8
sergiojavierre <sergio.javierre@fppiramide.com>
2025-02-27 08:37:57 +0100

    apañado

--- src/main.jsx
+++ src/main.jsx
@@ -8,23 +8,20 @@ import Auth from "./components/auth";
 import { UserContextProvider } from "./context/UserContext";
 import Usuario from "./components/usuario";
 import Conserje from "./components/conserje";
-import { ThemeContextProvider } from "./context/ThemeContext";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <UserContextProvider>
-    <ThemeContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="auth" element={<Auth />} />
-            <Route path="usuario" element={<Usuario />} />
-            <Route path="admin" element={<Conserje />} />
-            <Route path="*" element={<Auth />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="auth" element={<Auth />} />
+          <Route path="usuario" element={<Usuario />} />
+          <Route path="admin" element={<Conserje />} />
+          <Route path="*" element={<Auth />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </UserContextProvider>
 );
